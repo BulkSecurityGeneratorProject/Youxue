7ffe22f6d40ef94151ae2ff1efed9b2a4a3ad6c4
zhu04303661 <zhu04303661@163.com>
2018-05-14 16:20:49 +0800

    fix admin fail login issue

--- backend/src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ backend/src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -72,8 +72,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/i18n/**")
             .antMatchers("/content/**")
             .antMatchers("/swagger-ui/index.html")
-            .antMatchers("/test/**")
-            .antMatchers("/api/**");
+            .antMatchers("/test/**");
+//            .antMatchers("/api/**");
     }
 
     @Override
